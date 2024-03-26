a442dfcffee43deec8a9ef2e0b4a29f73d7e8b7a
Sayem <mail@sayem.dev>
2024-03-26 18:25:33 +0600

    Finished translation

--- assets/js/locale-data.js
+++ assets/js/locale-data.js
@@ -1,11 +1,205 @@
 const languages = {
     en: {
-        cTitle: "Business will more more profitable",
-        description: "This is the English version of the landing page."
+        cTitle: "Business will more <span>profitable</span>",
+        cShortDesc: `Shake off the decay of bygone days. Embark on a rejuvenated life with the advent of accountancy. 
+        Buying and selling, accounting, alerting to pending dues, tracking sales records, managing product stock - all together.`,
+        btnSeeHow: "See How",
+        tagSalesSoftware: "Sales Software (CRM)",
+        tagPosSystem: "POS System",
+        tagInventory: "Inventory/Stock Management",
+        fTitle: "What's in Cognito Software?",
+        fDesc: "How can you make your business more profitable using Cognito?",
+        f1title1: "Smart Selling: The taste of freedom!",
+        f1li1: "You can do your daily buying and selling through this software, very easily, with few clicks or barcode scanner.",
+        f1li2: "Remove the hassle of accounting, the software will take care of your accounting.",
+        f1li3: "You can provide receipts to customers per sale",
+
+        f2title1: "Product Stock: Never forget",
+        f2li1: "At any time, you can see how much stock of any product is in your shop.",
+        f2li2: "Which product needs to be stocked.",
+        f2li3: "See at a glance which product is selling how much, all at once.",
+
+        f3title1: "Worried about pending dues? No more!",
+        f3li1: "If you sell on credit, auto SMS alert will be sent to the customer's phone.",
+        f3li2: "Keep records of your customers easily.",
+        f3li3: "Notify customers at any time to advertise products.",
+        f3li4: "Want to check the previous transaction of the customer before selling? No more worries!",
+
+        f4title1: "Are you running a business with salesmen?",
+        f4li1: "Software prevents human irregularities.",
+        f4li2: "Your employees aren't doing their job properly? Monitor from anywhere.",
+        f4li3: "Set salesmen's targets, track how many of them have been fulfilled, now at your fingertips.",
+        f4li4: "Daily sales report by email.",
+
+        f5title1: "Need sales forecast?",
+        f5li1: "Forecast of next month's sales, so that more profitable products can be stocked.",
+        f5li2: "No need to do calculations to find out the most profitable products, cognito will prepare them for you.",
+        f5li3: "Details of business growth, customer status, keeping records of good and bad customers, everything is there.",
+        f5li4: "Send SMS notifications to advertise at any time.",
+
+        contactTitle: "Want to see a demo?",
+        contactDesc: `Want to try it yourself? 
+                        <br/>Or want to know how Cognito makes your business
+                        <br/> more profitable?`,
+        contactBtn: "Contact Us",
+
+        cnTitle: "Our goal is to help your business grow",
+        cnDesc: `To make the right decision, you need all kinds of information presented to you easily. 
+                    We are committed to making the place where you make this decision easy.`,
+        cn1Title: "Business-friendly Dashboard",
+        cn1Desc: `To make the right decision, you need all kinds of information presented to you easily in our dashboard.<br/>
+                                Stock is out for certain products? Just look at the dashboard<br/>
+                                Which products are more profitable for your business? or which products can sell better in future? 
+                                See everything at a glance, make decisions based on accurate information.`,
+        cn2Title: "Sales Report",
+        cn2Desc: `You'll find daily sales report throw email. Sales volume for each salesperson, achievements for their sales targets,
+                                sales based on products, due report etc<br/>
+                                Lots of new reports are being added to make it easier for you to make decisions.`,
+
+
+        monthly1: "/monthly",
+        monthly2: "/monthly",
+        contact: "Contact",
+
+        chooseBtnAtomic: "Choose",
+        chooseBtnPlanetary: "Choose",
+        chooseBtnCosmic: "Choose",
+
+        atomic: "Atomic",
+        fOrg1: "Organization: <span>1</span>",
+        fShop1: "Shops: <span>1</span>",
+        fSms1: "Free SMS <span>5/per month (Refill Available)</span>",
+        fEmail1: "Free Email <span>10/per month</span>",
+        fTnxLimit1: "Transaction Limit <span>$10k/per month</span>",
+
+        planetary: "Planetary",
+        fOrg2: "Organization: <span>1</span>",
+        fShop2: "Shops: <span>2</span>",
+        fSms2: "Free SMS <span>100/per month (Refill Available)</span>",
+        fEmail2: "Free Email <span>200 /per month</span>",
+        fTnxLimit2: "Transaction Limit <span>$1m/per day</span>",
+
+        cosmic: "Cosmic",
+        fOrg3: "Organization: <span>Unlimited</span>",
+        fShop3: "Shops: <span>Unlimited</span>",
+        fSms3: "Free SMS <span>500/per month (Refill Available)</span>",
+        fEmail3: "Free Email <span>Unlimited</span>",
+        fTnxLimit3: "Transaction Limit <span>Unlimited</span>",
+
+        bTitle: "Business software needs reliability",
+        bDesc: `Every system of any organization must be reliable and secure. Cognito's every feature goes through a rigorous quality control process.
+                    That's why Cognito is a system that is extremely reliable for your business.
+                    In addition, our support is available 24 hours a day at your doorstep for any need at any time.`,
+        package: "Package",
+        packageSelect: "Select Package based on your business needs",
+
+        navHome: "Home",
+        navFeatures: "Features",
+        navPackages: "Packages",
+        navContact: "Contact",
+        navLogin: "Login",
     },
     bn: {
-        title: "ব্যাবসা হবে আরো বেশি লাভজনক",
-        description: "Ceci est la version française de la page d'accueil."
+        cTitle: "ব্যাবসা হবে আরো বেশি <span>লাভজনক</span>",
+        cShortDesc: `ঝেড়ে ফেলুন পুরানো দিনের জীর্নতা। হিসেব নিকেশের যাতাকলে নিষ্পেষিত জীবনে শুরু করুন নতুন অধ্যায়।
+                        বেচাকেনা, হিসেব-নিকেশ, বাকি তুলতে অ্যালার্ট, সেলসম্যানের বিক্রির রেকর্ড, পণ্যের স্টক ম্যানেজ
+                        করা, সব একসাথে।`,
+        btnSeeHow: "দেখুন কিভাবে",
+        tagSalesSoftware: "সেলস সফটওয়্যার (সিআরএম)",
+        tagPosSystem: "পজ সিস্টেম",
+        tagInventory: "ইনভেন্টরি/স্টক ম্যানেজমেন্ট",
+        fTitle: "কি আছে কগনিটো সফটওয়্যারে?",
+        fDesc: "কিভাবে আপনার ব্যবসাকে আরো বেশি লাভজনক করতে পারে?",
+        f1title1: "স্মার্ট বেচাকেনাঃ মুক্তির স্বাদ!",
+        f1li1: "আপনার যাবতীয় বেচাকেনা করতে পারবেন এই সফটওয়্যারের মাধ্যমে, খুব সহজে, কয়েক ক্লিকে অথবা বারকোড স্ক্যানারের মাধ্যমে।",
+        f1li2: "হিসেব নিকেশের ঝামেলা থেকে মুক্তি, সফটওয়্যার করবে বেচাকেনার হিসেব।",
+        f1li3: "চাইলে কাস্টমারদের রিসিপ্ট দিতে পারবেন প্রতি সেলে।",
+
+        f2title1: "পন্য স্টকঃ ভুলে যাওয়া বারন",
+        f2li1: "আপনার শপে যেকোন মুহুর্তে, যেকোন প্রোডাক্টের কি পরিমান স্টক আছে।",
+        f2li2: "কোনটা স্টক করা প্রয়োজন।",
+        f2li3: "কোন প্রোডাক্ট কি পরিমান বিক্রি হচ্ছে সব দেখুন এক নজরে, একসাথে।",
+
+        f3title1: "বাকি তোলার চিন্তা? আর না",
+        f3li1: "বাকিতে বিক্রি করলে অটো এসএমএস অ্যালার্ট যাবে কাস্টমারের ফোনে।",
+        f3li2: "আপনার কাস্টমারদের রেকর্ড রাখুন সযতনে।",
+        f3li3: "যেকোন মুহুর্তে কাস্টমাদেরকে নোটিফিকেশন পাঠান প্রোডাক্টের বিজ্ঞাপন দিতে।",
+        f3li4: "বিক্রির আগে যাচাই করতে চান কাস্টমারের পূর্বের আমলনামা? চিন্তা আর না!",
+
+        f4title1: "সেলসম্যান রেখে ব্যবসা করছেন?",
+        f4li1: "অসাধুতা রুখতে সফটওয়্যারের বিকল্প নেই",
+        f4li2: "কাজে ফাঁকি? মনিটর করুন যেকোন জায়গা থেকে।",
+        f4li3: "সেলসম্যানের টার্গেট সেট করা, তার কতখানি পূরণ হয়েছে সেগুলো ট্র্যাক রাখা এখন হাতের মুঠোয়।",
+        f4li4: "ইমেইলে দৈনিক সেলস রিপোর্ট।",
+
+        f5title1: "ভবিষ্যতে কোন পণ্য বেশি বিক্রি হতে পারে?",
+        f5li1: "আগামি মাসের আগাম পূর্বাভাস, যাতে বেশি লাভজনক পণ্য স্টক করা যায়।",
+        f5li2: "সর্বাধিক লাভজনক পণ্য হিসেব করে বের করার প্রয়োজন নেই, সফটওয়্যারেই দেখে নিন।",
+        f5li3: "ব্যবসার উন্নতির বিবরণ, কাষ্টমারদের অবস্থা, ভাল-খারাপ কাষ্টমারের রেকর্ড রাখা সবই আছে।",
+        f5li4: "এসএমএস নোটিফিকেশন পাঠিয়ে বিজ্ঞাপন দিন যেকোন মুহূর্তে।",
+
+        contactTitle: "ডেমো দেখতে চান?",
+        contactDesc: "নিজে ট্রাই করে দেখতে চান? <br/>কিংবা কিভাবে আপনার ব্যবসাকে কগনিটো সফটওয়্যার<br/> দিয়ে আরো লাভজনক করতে পারবেন তা বিস্তারিত জানতে চান?",
+        contactBtn: "যোগাযোগ করুন",
+
+        cnTitle: "আমাদের প্রধান লক্ষ্য আপনার ব্যবসা বৃদ্ধিতে সহযোগিতা করা",
+        cnDesc: "ব্যবসাকে আরও বড় এবং বেশি লাভজনক করতে প্রয়োজন সঠিক সময়ে সঠিক সিদ্ধান্ত নেয়া। আমরা আপনার এ সিদ্ধান্ত নেয়ার জায়গাটা সহজ করার চেষ্টায় নিয়োজিত।",
+        cn1Title: "ব্যবসা-বান্ধব ড্যাশবোর্ড",
+        cn1Desc: `সঠিক সিদ্ধান্ত নিতে যত ধরনের তথ্য প্রয়োজন, সহজভাবে আপনার কাছে উপস্থাপন করার জন্য আছে
+                                আমাদের ড্যাশবোর্ড।<br/>
+                                কোন পণ্যের স্টক কমে গিয়েছে? ড্যাশবোর্ডেই দেখে নিন এক নজরে।<br/>
+                                কোন পণ্যটি আপনার জন্য বেশি লাভজনক? কিংবা ভবিষ্যতে কোন পণ্য বেশি বিক্রি হতে পারে? সব
+                                দেখুন এক নজরে, সিদ্ধান্ত নিন সঠিক তথ্যের উপর নির্ভর করে।`,
+        cn2Title: "সেলস রিপোর্ট",
+        cn2Desc: `ইমেইলে পাবেন দৈনিক সেলস রিপোর্ট। কোন সেলসম্যান কতটুকু বিক্রি করেছে, টার্গেটের কতখানি
+                                পূরণ হয়েছে, কোন পণ্যে কত বিক্রি হয়েছে, কোন কাস্টমার কত টাকা বাকি আছে, সব।<br/>
+                                সেই সাথে প্রতিনিয়ত আর নতুন নতুন রিপোর্ট যুক্ত হচ্ছে আপনার সিদ্ধান্ত নেয়ার পদ্ধতি সহজ
+                                করার প্রয়োজনে।`,
+
+
+        monthly1: "/মাসিক",
+        monthly2: "/মাসিক",
+        contact: "যোগাযোগ",
+
+        chooseBtnAtomic: "পছন্দ করুন",
+        chooseBtnPlanetary: "পছন্দ করুন",
+        chooseBtnCosmic: "পছন্দ করুন",
+
+        atomic: "আণবিক",
+        fOrg1: "প্রতিষ্ঠানঃ <span>১ টি</span>",
+        fShop1: "শপঃ <span>১ টি</span>",
+        fSms1: "ফ্রি এসএম এসঃ <span>৫ টি/মাসিক (রিফিল সহজলভ্য)</span>",
+        fEmail1: "ফ্রি ইমেইলঃ <span>১০ টি/মাসিক</span>",
+        fTnxLimit1: "ট্রানজেকশন লিমিটঃ <span>১০,০০০/দৈনিক</span>",
+
+        planetary: "প্ল্যানেটারি",
+        fOrg2: "প্রতিষ্ঠানঃ <span>১ টি</span>",
+        fShop2: "শপঃ <span>২ টি</span>",
+        fSms2: "ফ্রি এসএম এসঃ <span>১০০ টি/মাসিক (রিফিল সহজলভ্য)</span>",
+        fEmail2: "ফ্রি ইমেইলঃ <span>২০০ টি/মাসিক</span>",
+        fTnxLimit2: "ট্রানজেকশন লিমিটঃ <span>১০,০০,০০০/দিন</span>",
+
+        cosmic: "কসমিক",
+        fOrg3: "প্রতিষ্ঠানঃ <span>আনলিমিটেড</span>",
+        fShop3: "শপঃ <span>আনলিমিটেড</span>",
+        fSms3: "ফ্রি এসএম এসঃ <span>৫০০ টি/মাসিক (রিফিল সহজলভ্য)</span>",
+        fEmail3: "ফ্রি ইমেইলঃ <span>আনলিমিটেড</span>",
+        fTnxLimit3: "ট্রানজেকশন লিমিটঃ <span>আনলিমিটেড</span>",
+
+        bTitle: "ব্যবসার সফটওয়্যারে প্রয়োজন নির্ভরযোগ্যতা",
+        bDesc: `যেকোন প্রতিষ্ঠানের সিস্টেম হতে হবে নির্ভরযোগ্য এবং সুরক্ষিত।
+                    কগনিটোর প্রতিটি ফিচার কঠোর মান-নিয়ন্ত্রন প্রসেসের মধ্যে দিয়ে যায়। কাজেই কগনিটো আপনার ব্যবসার জন্য
+                    অত্যান্ত নির্ভরযোগ্যতা একটি সিস্টেম।
+                    তাছাড়া যেকোন মুহুর্তে যেকোন প্রয়োজনে ২৪ ঘণ্টা আমাদের সাপোর্ট আপনাদের দোরগোড়ায়।`,
+
+        package: "প্যাকেজ",
+        packageSelect: "আপনার ব্যবসার প্রয়োজন অনুযায়ী যেকোন প্যাকেজ নির্বাচন করুন।",
+
+        navHome: "হোম",
+        navFeatures: "ফিচারগুলো",
+        navPackages: "প্যাকেজ",
+        navContact: "যোগাযোগ",
+        navLogin: "লগিন",
     }
 };
 
@@ -21,4 +215,90 @@ function resolve(lang, key) {
     }
 }
 
-document.getElementById("cTitle").innerText = resolve(lang, "cTitle")
+console.log(lang)
+
+document.getElementById("cTitle").innerHTML = resolve(lang, "cTitle")
+document.getElementById("cShortDesc").innerHTML = resolve(lang, "cShortDesc")
+
+document.getElementById("btnSeeHow").innerHTML = resolve(lang, "btnSeeHow")
+document.getElementById("tagSalesSoftware").innerHTML = resolve(lang, "tagSalesSoftware")
+document.getElementById("tagPosSystem").innerHTML = resolve(lang, "tagPosSystem")
+document.getElementById("tagInventory").innerHTML = resolve(lang, "tagInventory")
+document.getElementById("fTitle").innerHTML = resolve(lang, "fTitle")
+document.getElementById("fDesc").innerHTML = resolve(lang, "fDesc")
+document.getElementById("f1title1").innerHTML = resolve(lang, "f1title1")
+document.getElementById("f1li1").innerHTML = resolve(lang, "f1li1")
+document.getElementById("f1li2").innerHTML = resolve(lang, "f1li2")
+document.getElementById("f1li3").innerHTML = resolve(lang, "f1li3")
+document.getElementById("f2title1").innerHTML = resolve(lang, "f2title1")
+document.getElementById("f2li1").innerHTML = resolve(lang, "f2li1")
+document.getElementById("f2li2").innerHTML = resolve(lang, "f2li2")
+document.getElementById("f2li3").innerHTML = resolve(lang, "f2li3")
+document.getElementById("f3title1").innerHTML = resolve(lang, "f3title1")
+document.getElementById("f3li1").innerHTML = resolve(lang, "f3li1")
+document.getElementById("f3li2").innerHTML = resolve(lang, "f3li2")
+document.getElementById("f3li3").innerHTML = resolve(lang, "f3li3")
+document.getElementById("f3li4").innerHTML = resolve(lang, "f3li4")
+document.getElementById("f4title1").innerHTML = resolve(lang, "f4title1")
+document.getElementById("f4li1").innerHTML = resolve(lang, "f4li1")
+document.getElementById("f4li2").innerHTML = resolve(lang, "f4li2")
+document.getElementById("f4li3").innerHTML = resolve(lang, "f4li3")
+document.getElementById("f4li4").innerHTML = resolve(lang, "f4li4")
+document.getElementById("f5title1").innerHTML = resolve(lang, "f5title1")
+document.getElementById("f5li1").innerHTML = resolve(lang, "f5li1")
+document.getElementById("f5li2").innerHTML = resolve(lang, "f5li2")
+document.getElementById("f5li3").innerHTML = resolve(lang, "f5li3")
+document.getElementById("f5li4").innerHTML = resolve(lang, "f5li4")
+document.getElementById("contactTitle").innerHTML = resolve(lang, "contactTitle")
+document.getElementById("contactDesc").innerHTML = resolve(lang, "contactDesc")
+document.getElementById("contactBtn").innerHTML = resolve(lang, "contactBtn")
+document.getElementById("cnTitle").innerHTML = resolve(lang, "cnTitle")
+document.getElementById("cnDesc").innerHTML = resolve(lang, "cnDesc")
+document.getElementById("cn1Title").innerHTML = resolve(lang, "cn1Title")
+document.getElementById("cn1Desc").innerHTML = resolve(lang, "cn1Desc")
+document.getElementById("cn2Title").innerHTML = resolve(lang, "cn2Title")
+document.getElementById("cn2Desc").innerHTML = resolve(lang, "cn2Desc")
+
+const monthly = document.getElementsByClassName("monthly")
+for (let i = 0; i < monthly.length; i++) {
+    console.log(monthly[i])
+    monthly.innerHTML = resolve(lang, "monthly")
+}
+document.getElementById("atomic").innerHTML = resolve(lang, "atomic")
+document.getElementById("fOrg1").innerHTML = resolve(lang, "fOrg1")
+document.getElementById("fShop1").innerHTML = resolve(lang, "fShop1")
+document.getElementById("fSms1").innerHTML = resolve(lang, "fSms1")
+document.getElementById("fEmail1").innerHTML = resolve(lang, "fEmail1")
+document.getElementById("fTnxLimit1").innerHTML = resolve(lang, "fTnxLimit1")
+
+document.getElementById("planetary").innerHTML = resolve(lang, "planetary")
+document.getElementById("fOrg2").innerHTML = resolve(lang, "fOrg2")
+document.getElementById("fShop2").innerHTML = resolve(lang, "fShop2")
+document.getElementById("fSms2").innerHTML = resolve(lang, "fSms2")
+document.getElementById("fEmail2").innerHTML = resolve(lang, "fEmail2")
+document.getElementById("fTnxLimit2").innerHTML = resolve(lang, "fTnxLimit2")
+
+document.getElementById("cosmic").innerHTML = resolve(lang, "cosmic")
+document.getElementById("fOrg3").innerHTML = resolve(lang, "fOrg3")
+document.getElementById("fShop3").innerHTML = resolve(lang, "fShop3")
+document.getElementById("fSms3").innerHTML = resolve(lang, "fSms3")
+document.getElementById("fEmail3").innerHTML = resolve(lang, "fEmail3")
+document.getElementById("fTnxLimit3").innerHTML = resolve(lang, "fTnxLimit3")
+
+document.getElementById("bTitle").innerHTML = resolve(lang, "bTitle")
+document.getElementById("bDesc").innerHTML = resolve(lang, "bDesc")
+
+document.getElementById("package").innerHTML = resolve(lang, "package")
+document.getElementById("packageSelect").innerHTML = resolve(lang, "packageSelect")
+document.getElementById("contact").innerHTML = resolve(lang, "contact")
+document.getElementById("monthly1").innerHTML = resolve(lang, "monthly1")
+document.getElementById("monthly2").innerHTML = resolve(lang, "monthly2")
+document.getElementById("chooseBtnAtomic").innerHTML = resolve(lang, "chooseBtnAtomic")
+document.getElementById("chooseBtnPlanetary").innerHTML = resolve(lang, "chooseBtnPlanetary")
+document.getElementById("chooseBtnCosmic").innerHTML = resolve(lang, "chooseBtnCosmic")
+
+document.getElementById("navHome").innerHTML = resolve(lang, "navHome")
+document.getElementById("navFeatures").innerHTML = resolve(lang, "navFeatures")
+document.getElementById("navPackages").innerHTML = resolve(lang, "navPackages")
+document.getElementById("navContact").innerHTML = resolve(lang, "navContact")
+document.getElementById("navLogin").innerHTML = resolve(lang, "navLogin")
